f09f4b43a8dfc98933d8ad2bba7d459f01cd2023
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2023-09-06 19:44:56 -0600

    actualizando echo

--- main.js
+++ main.js
@@ -34,21 +34,21 @@ function main() {
     // Leer la opcion del usuario y llamar la funcion correspondente
     rl.question('Opcion -> ', answer => {
         switch (answer) {
-          case '1':
-              loginMain();
-              break;
-          case '2':
-              logoutMain();
-              break;
-          case '3':
-              console.log('\nGracias por usar alumchat. Vuelva pronto!');
-              rl.close();
-              break;
-          default:
-              console.log('Opcion invalida! Intente de nuevo!');
-              main();
+            case '1':
+                loginMain();
+                break;
+            case '2':
+                logoutMain();
+                break;
+            case '3':
+                console.log('\nGracias por usar alumchat. Vuelva pronto!');
+                rl.close();
+                break;
+            default:
+                console.log('Opcion invalida! Intente de nuevo!');
+                main();
         }
-      });
+    });
 }
 
 /**
@@ -95,7 +95,7 @@ function submenuDV() {
                 console.log('Opcion invalida! Intente de nuevo!');
                 submenuDV();
         }
-    }); 
+    });
 }
 
 /**
@@ -104,21 +104,21 @@ function submenuDV() {
 async function loginMain() {
     console.log('\nINICIAR SESION:')
     rl.question('Usuario: ', async username => {
-      rl.question('Contraseña: ', async password => {
-        try {
-            await client.login(username, password); //esperando a la funcion login()
-            console.log('\nSesion iniciada exitosamente!');
-            console.log('Bienvenido de nuevo, ' + username + '!');
-            messageListener();
-            menu(); //redigiendo a menu()
-        } catch (err) {
-            // Si hay un error, se muestra en pantalla y se vuelve a llamar a login()
-            console.log(err.message)
-            loginMain();
-        }
-      });
+        rl.question('Contraseña: ', async password => {
+            try {
+                await client.login(username, password); //esperando a la funcion login()
+                console.log('\nSesion iniciada exitosamente!');
+                console.log('Bienvenido de nuevo, ' + username + '!');
+                messageListener();
+                menu(); //redigiendo a menu()
+            } catch (err) {
+                // Si hay un error, se muestra en pantalla y se vuelve a llamar a login()
+                console.log(err.message)
+                loginMain();
+            }
+        });
     });
-  }
+}
 
 function setUpId(config) {
     console.log(`Names:`, config);
@@ -137,8 +137,10 @@ function setUpIdNeighbors(config) {
     const neighbors = config[searchKey];
     for (let neighbor of neighbors) {
         client.router.addNeighbor(neighbor);
+        client.router.routingTable[neighbor] = -1;
     }
     console.log(`Neighbors:`, client.router.neighbors);
+    console.log(`Routing Table:`, client.router.routingTable);
 }
 
 async function sendEcho() {
@@ -146,27 +148,27 @@ async function sendEcho() {
     const echoMessage = {
         type: "echo",
         headers: {
-          from: `${client.username}@${client.domain}`, 
-          to: "", 
+            from: `${client.username}@${client.domain}`,
+            to: "",
         },
         payload: {
-          timestamp1: "",
-          timestamp2: "",
+            timestamp1: "",
+            timestamp2: "",
         },
-      };
+    };
 
     const neighbors = client.router.neighbors;
-    
+
     for (let neighbor of neighbors) {
         const neighborJid = client.names[neighbor];
-        const echoMessageCopy = {...echoMessage};
+        const echoMessageCopy = { ...echoMessage };
         echoMessageCopy.headers.to = neighborJid;
 
         const timestamp1 = Date.now();
         echoMessageCopy.payload.timestamp1 = timestamp1;
         try {
             client.directMessage(neighborJid, JSON.stringify(echoMessageCopy));
-            console.log(`Sending echo message from Router ${client.username + ", "  + client.router.id} to Router ${neighborJid + ", " + neighbor}`)
+            console.log(`Sending echo message from Router ${client.username + ", " + client.router.id} to Router ${neighborJid + ", " + neighbor}`)
         }
         catch (err) {
             console.log(err.message);
@@ -175,39 +177,61 @@ async function sendEcho() {
 }
 
 function updateNeighbors(messageData) {
-    const timeDiff = "";
-    if(messageData.headers.to === `${client.username}@${client.domain}`) {
+    console.log("object:", messageData);
+    let timeDiff = 0;
+    if (messageData.headers.to === `${client.username}@${client.domain}`) {
         const user = messageData.headers.from;
         const timestamp2 = Date.now();
-        const echoMessageCopy = {...messageData};
+        const echoMessageCopy = { ...messageData };
 
         echoMessageCopy.payload.timestamp2 = timestamp2;
-        timeDiff = echoMessageCopy.payload.timestamp2 - echoMessageCopy.payload.timestamp1;
+        timeDiff = echoMessageCopy.payload.timestamp2 - parseInt(echoMessageCopy.payload.timestamp1);
         console.log(`Echo message from Router ${echoMessageCopy.headers.from} to Router ${echoMessageCopy.headers.to} took ${timeDiff} ms`);
-        client.directMessage(user, JSON.stringify(echoMessageCopy));   
+        client.directMessage(user, JSON.stringify(echoMessageCopy));
+
+        // Actualizar tabla de enrutamiento
+        // Buscamos la clave del usuario en la tabla de enrutamiento
+
+        const nodeId = Object.keys(client.names).find(key => client.names[key] === user);
+
+        // Actualizamos la tabla de enrutamiento
+        client.router.routingTable[nodeId] = timeDiff;
+
+        console.log(`Routing Table Actualizada:`, client.router.routingTable);
+
     } else {
-        const timestamp1 = messageData.payload.timestamp1;
-        const timestamp2 = messageData.payload.timestamp2;
+        const timestamp1 = parseInt(messageData.payload.timestamp1);
+        const timestamp2 = parseInt(messageData.payload.timestamp2);
         timeDiff = timestamp2 - timestamp1;
         console.log(`Echo message from Router ${messageData.headers.from} to Router ${messageData.headers.to} took ${timeDiff} ms`);
+
+        // Actualizar tabla de enrutamiento
+        // Buscamos la clave del usuario en la tabla de enrutamiento
+
+        const nodeId = Object.keys(client.names).find(key => client.names[key] === messageData.headers.to);
+
+        // Actualizamos la tabla de enrutamiento
+        client.router.routingTable[nodeId] = timeDiff;
+
+        console.log(`Routing Table Actualizada:`, client.router.routingTable);
     }
 
 }
 
 function messageListener() {
-    if(!client.xmpp) {
+    if (!client.xmpp) {
         throw new Error("Error en la conexion, intenta de nuevo.");
     }
 
     client.xmpp.on("stanza", async (stanza) => {
         // console.log(stanza.toString());
-        if(stanza.is("message")) {
+        if (stanza.is("message")) {
             // console.log(stanza.toString());
             const type = stanza.attrs.type;
             const from = stanza.attrs.from.split("/")[0];
             const body = stanza.getChildText("body");
 
-            if(type === "chat" && body) {
+            if (type === "chat" && body) {
                 console.log(`\n${from}: ${body}`);
                 try {
                     const messageData = JSON.parse(body);
@@ -217,16 +241,16 @@ function messageListener() {
                     else if (messageData.type === "topo" && messageData.config) {
                         setUpIdNeighbors(messageData.config);
                     }
-                    else if (messageData.type === "message" && messageData.headers && messageData.paylod) {
-                        checkMessage(messageData.headers, messageData.paylod);
+                    else if (messageData.type === "message" && messageData.headers && messageData.payload) {
+                        checkMessage(messageData.headers, messageData.payload);
                     }
-                    else if (messageData.type === "info" && messageData.headers && messageData.paylod) {
-                        updateRoutingTable(messageData.headers, messageData.paylod);
+                    else if (messageData.type === "info" && messageData.headers && messageData.payload) {
+                        updateRoutingTable(messageData.headers, messageData.payload);
                     }
-                    else if (messageData.type === "echo" && messageData.headers && messageData.paylod) {
+                    else if (messageData.type === "echo" && messageData.headers && messageData.payload) {
                         updateNeighbors(messageData);
                     }
-                    
+
                 } catch (error) {
                     console.error(`Error parsing JSON: ${error}`);
                 }
